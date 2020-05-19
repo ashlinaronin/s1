70cf7e5c8214163c3fa51959b872350895e44911
ashlinaronin <ashlinaronin@gmail.com>
2020-05-18 22:03:15 -0700

    Move fundraiser video to StreamingAssets and try to get it to play in WebGL build

---
+++ Assets/PlayStreamingVideoWebGL.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayStreamingVideoWebGL : MonoBehaviour
+{
+    [Tooltip("name of a file in the StreamingAssets folder to play")]
+    public string filename;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.url = System.IO.Path.Combine(Application.streamingAssetsPath, filename); 
+        player.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
